9ef074263f08ebbca4891c8323d5d197a5a51982
duc <ducnguy@stanford.edu>
2015-04-04 14:04:11 -0700

    SOUND WORKING\!\!\!

--- js/userInteraction.js
+++ js/userInteraction.js
@@ -32,7 +32,29 @@ function elizaStep(str,target) {
 		ericTxtObj.text(rpl)
 		ericTxtObj.focus()
 		ericTxtObj.fadeIn(400)
+        sayEricLine(rpl)
 	})
 	//elizaLines.push(usr);
 	//elizaLines.push(rpl);
+}
+var TTS_URL = "https://t2s.p.mashape.com/speech/"
+function sayEricLine(str){
+    var context = new AudioContext();
+    var req = new XMLHttpRequest();
+    console.log(str)
+    console.log(encodeURI(str))
+    var args = "lang=en&text="+encodeURI(str)
+    req.responseType = "arraybuffer"
+    req.onload = function (){
+        context.decodeAudioData(req.response, function (buffer){
+            var playSound = context.createBufferSource();
+            playSound.buffer = buffer;
+            playSound.connect(context.destination);
+            playSound.start(0);
+        });
+    }
+    req.open('POST',TTS_URL, true)
+    req.setRequestHeader("X-Mashape-Key","5AmPnlra6YmshRKeIg9EhGMiqLPJp14k775jsnsDgfiv5yb2u4")
+    req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+    req.send(args)
 }
