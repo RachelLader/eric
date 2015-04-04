381acf1937d48fdd776c17f306204e1f0e7fad26
duc <ducnguy@stanford.edu>
2015-04-04 12:54:13 -0700

    LEAP MOTION AND ELIZA WORKING\!\!\! WOOT WOOT WOOT \!\!\!

--- js/userInteraction.js
+++ js/userInteraction.js
@@ -1,4 +1,38 @@
 window.onload = function(e){ 
-    document.getElementById('userSays').contentEditable = true
+    document.getElementById('userSays').contentEditable = true;
+    initEliza()
+    userTxtObj = $('#userSays')
+    userTxtObj.focus();
+    userTxtObj.keydown(function(e){
+    	if(e.which == 13) {
+    		e.preventDefault();
+        	console.log(e.currentTarget.textContent);
+        	elizaStep(e.currentTarget.textContent)
+        	userTxtObj.fadeOut(400,function(){
+        		userTxtObj.text("")
+        		userTxtObj.focus()
+        		userTxtObj.fadeIn(0)
+        	})
+        }
+    })
+}
 
+var eliza
+function initEliza(){
+	eliza = new ElizaBot();
+}
+
+function elizaStep(str,target) {
+	console.log('step')
+	var userinput = str
+	var rpl = eliza.transform(userinput);
+	console.log(rpl)
+	ericTxtObj = $('#ericSays')
+	ericTxtObj.fadeOut(400,function(){
+		ericTxtObj.text(rpl)
+		ericTxtObj.focus()
+		ericTxtObj.fadeIn(400)
+	})
+	//elizaLines.push(usr);
+	//elizaLines.push(rpl);
 }
